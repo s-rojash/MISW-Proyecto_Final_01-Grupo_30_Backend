63d3e0e822430c66b9813ef9d1b3b85d31a75305
Oscar Alvarez <o.alvareze@uniandes.edu.co>
2023-10-28 13:44:49 -0500

    Ajustes CORS

--- banco-preguntas/src/main/java/com/proyecto/bancopreguntas/controller/BancoPreguntasController.java
+++ banco-preguntas/src/main/java/com/proyecto/bancopreguntas/controller/BancoPreguntasController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/banco-preguntas")
 public class BancoPreguntasController {
